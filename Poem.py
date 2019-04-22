ba8bc595ec09cda4aa9b316a87163fbfdd4abcd6
Khann Mean <49180832+khmean@users.noreply.github.com>
2019-04-22 16:24:12 -0700

    Add files via upload

--- Poem.py
+++ Poem.py
@@ -6,12 +6,15 @@ This program will define a poem class.
 
 
 class Poem(object):
+    # Initialize the number of poems to 0
+    num_of_poems = 0
 
     def __init__(self, author, title):
         """Constructor creates a Poem with author's name and title of the poem"""
-
         self.author = author
         self.title = title
+        # Increment the amount of Poems used
+        Poem.num_of_poems += 1
 
     def getAuthor(self):
         """Returns the authors name"""
@@ -25,7 +28,6 @@ class Poem(object):
         """Set the content of the poem"""
         self.content = content
 
-
     def getContent(self):
         """Return the content of the poem"""
         return self.content
@@ -35,28 +37,35 @@ class Poem(object):
 
 
 def main():
-
+    # Assign the first poem to class Poem
     p1 = Poem("John Doe", "Title")
     p1contentlist = []
     p1contentlist.append("This is the first line")
     p1contentlist.append("This is the second line")
     p1.setContent(p1contentlist)
 
+    # Assign the second poem to class Poem
     p2 = Poem("Jane Doe", "Title 2")
     p2contentlist = []
     p2contentlist.append("This is the first line")
     p2contentlist.append("This is the second line")
+    p2contentlist.append("This is the third line")
     p2.setContent(p2contentlist)
 
+    # Assign the third poem to class Poem
     p3 = Poem("Jay Doe", " Title 3")
     p3contentlist = []
     p3contentlist.append("This is the first line")
     p3contentlist.append("This is the second line")
+    p3contentlist.append("This is the third line")
+    p3contentlist.append("This is the fourth line")
     p3.setContent(p3contentlist)
 
     print(p1)
     print(p2)
     print(p3)
+
+    print("There was {}".format(Poem.num_of_poems) + " poems.")
 
 
 if __name__ == "__main__":
